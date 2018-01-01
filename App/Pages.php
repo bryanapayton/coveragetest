02e2fee4798dd2802ab938c74dd3213eae1b7770
root <root@ip-172-31-17-173.ec2.internal>
2018-01-01 15:08:12 +0000

    first test

---
+++ App/Pages.php
@@ -0,0 +1,18 @@
+<?php
+
+class Pages
+{
+public function render()
+{
+return 'Hello World';
+}
+public function returnTrue()
+{
+return true;
+}
+public function returnArray()
+{
+return ['Hello', 'World', 'This', 'Is', 'An', 'Array'];
+}
+}
+
---
+++ Test/App/PagesTest.php
@@ -0,0 +1,33 @@
+<?php
+require(__DIR__.'/../../App/Pages.php');
+class PagesTest extends PHPUnit\Framework\TestCase
+{
+
+public function testRenderReturnsHelloWorld()
+{
+
+$pages = new Pages();
+$expected = 'Hello World';
+$this->assertEquals($expected, $pages->render());
+
+}
+
+public function testReturnTrueReturnsTrue()
+{
+$pages = new Pages();
+$this->assertTrue($pages->returnTrue());
+}
+
+public function testReturnArrayReturnValidArray()
+{
+$pages = new Pages();
+$this->assertTrue(is_array($pages->returnArray()));
+}
+public function testReturnArrayReturnsNonEmptyArray()
+{
+$pages = new Pages();
+$this->assertTrue((count($pages->returnArray()) > 0));
+}
+
+}
+
